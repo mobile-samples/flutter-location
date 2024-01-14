c7640e0bbb0a607c163118507bbb175320c8fd7a
Hieu Vo <voquochieu1908@gmail.com>
2024-01-14 13:04:25 +0700

    Update http gelper

--- lib/utils/http_helper.dart
+++ lib/utils/http_helper.dart
@@ -1,40 +1,42 @@
-import 'dart:io' show Platform;
 import 'dart:convert';
-
+import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 
 class HttpHelper {
   HttpHelper._instantiate();
   static final HttpHelper instance = HttpHelper._instantiate();
-
-  final String baseUrlIOS = 'http://localhost:8082';
-  final String baseUrlAndroid = 'http://10.0.2.2:8082';
+  final storage = const FlutterSecureStorage();
+  final String baseUrl = 'http://localhost:8082';
+  final String baseAndroidUrl = 'http://10.0.2.2:8082';
 
   getUrl() {
-    if (Platform.isAndroid) {
-      return baseUrlAndroid;
-    } else if (Platform.isIOS) {
-      return baseUrlIOS;
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      return baseAndroidUrl;
+    } else {
+      return baseUrl;
     }
-    return 'http://localhost:8082';
+  }
+
+  setToken(String token) {
+    storage.write(key: "token", value: token);
   }
 
   jsonDecode(List<int> bodyBytes) {
-     String source = const Utf8Decoder().convert(bodyBytes);
-     return json.decode(source);
+    String source = const Utf8Decoder().convert(bodyBytes);
+    return json.decode(source);
   }
 
   buildHeader() async {
-    const storage = FlutterSecureStorage();
     final token = await storage.read(key: 'token') ?? '';
     if (token != '') {
       return {
         'Content-Type': 'application/json; charset=UTF-8',
-        'Authorization': 'Bearer $token',
+        'Authorization': 'Bearer $token'
       };
     }
     return {
-      'Content-Type': 'application/json; charset=UTF-8',
+      'Content-Type': 'application/json; charset=UTF-8'
     };
   }
 }
